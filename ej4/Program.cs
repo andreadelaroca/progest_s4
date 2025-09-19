b994ccff226a3831cc7240de7f96801d0ddd50b0
sar24001 <168153074+sar24001@users.noreply.github.com>
2025-09-19 10:55:00 -0600

    Add files via upload

---
+++ ej4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void CalcularIVAyTotal(int cantidad, double precioUnitario)
+    {
+        double subtotal = cantidad * precioUnitario;
+        double iva = subtotal * 0.15;
+        double total = subtotal + iva;
+        Console.WriteLine($"Cantidad de articulos: {cantidad}");
+        Console.WriteLine($"Precio unitario: {precioUnitario}");
+        Console.WriteLine($"El subtotal es: {subtotal}");
+        Console.WriteLine($"El IVA es: {iva}");
+        Console.WriteLine($"El total a pagar es: {total}");
+    }
+    
+    static void Main()
+    {
+        Console.WriteLine("Ingrese el total de articulos: ");
+        int cantidad = int.Parse(Console.ReadLine());
+        Console.Write("Ingrese el precio unitario del articulo: ");
+        double precioUnitario = Convert.ToDouble(Console.ReadLine());
+        CalcularIVAyTotal(cantidad, precioUnitario);
+    }
+}
