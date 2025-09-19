5cc655f22b6d3be7ec51c0f96cb534206c693793
Papaleta-ez <laguirrev@uamv.edu.ni>
2025-09-19 10:15:52 -0600

    Add files via upload

---
+++ ej3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+     static bool EsPar(int numero)
+    {
+        return numero % 2 == 0;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ejercicio 3: Determinar si el numero es par o impar.");
+        Console.Write("Ingresa un número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (EsPar(numero))
+        {
+            Console.WriteLine("El número es par.");
+        }
+        else
+        {
+            Console.WriteLine("El número es impar.");
+        }
+    }
+}
