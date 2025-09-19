3b48d6787703c33f733fb05e3fd64326df098807
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-09-19 10:29:38 -0600

    Cálculo de area, funciones

---
+++ calcarea/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    static double AreaRect(double x, double y)
+    {
+        double area = x * y;
+        return area;
+    }
+    static double AreaCirc(double x)
+    {
+        double area = Math.PI * (x * x);
+        return area;
+    }
+
+    static double AreaTrap(double x, double y, double z)
+    {
+        double area = ((x + y) * z) / 2;
+        return area;
+    }
+    private static void Main(string[] args)
+    {
+
+    }
+    }
